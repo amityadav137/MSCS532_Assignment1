a27490aac0fe9ff5ce5704d7bbebe8f0bc6930a5
amit <amit.yadav137@outlook.com>
2025-07-06 15:05:39 -0500

    2nd Input

--- insertion_sort.py
+++ insertion_sort.py
@@ -11,3 +11,8 @@ def insertion_sort_desc(arr):
 nums = [5, 2, 9, 1, 5, 6]
 insertion_sort_desc(nums)
 print("Sorted array (descending):", nums)
+
+# Example 2
+nums2 = [10, 20, 15, 30, 5]
+insertion_sort_desc(nums2)
+print("Sorted array (descending):", nums2)
